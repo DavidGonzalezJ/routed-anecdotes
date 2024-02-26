7ff7925f10c1b1587e565bf28ae13cc165dbe92e
David <davidgonzalezj97@gmail.com>
2024-02-26 12:13:49 +0100

    Adding a view for a single anecdote

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,30 @@
 import { useState } from 'react'
 import {
   BrowserRouter as Router,
-  Routes, Route, Link
+  Routes, Route, Link, useMatch
 } from 'react-router-dom'
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote =>
+        <li key={anecdote.id}>
+          <Link to={`/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>)}
     </ul>
   </div>
 )
+
+const Anecdote = ({anecdote}) => {
+  return(
+    <div>
+      <h2>{anecdote.content}</h2>
+      <p>has {anecdote.votes} votes</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -116,10 +129,14 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useMatch('/:id')
+  const anecdote = match
+    ? anecdotes.find(anecdote => anecdote.id === Number(match.params.id))
+    : null
+
   return (
     <>
     <h1>Software anecdotes</h1>
-    <Router>
       <div>
         <Link style={padding} to='/'>Anecdotes</Link>
         <Link style={padding} to='/create'>Create new</Link>
@@ -127,10 +144,10 @@ const App = () => {
       </div>
       <Routes>
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes}/>} />
+        <Route path='/:id' element={<Anecdote anecdote={anecdote}/>} />
         <Route path='/create' element={<CreateNew/>} />
         <Route path='/about' element={<About/>} />
       </Routes>
-    </Router>
     <div>
       <Footer />
     </div>
